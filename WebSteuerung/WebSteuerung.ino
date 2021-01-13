937f4b7a5ea9ecacec084197dc93f9108cc65287
Martin Lorenz <mlo@poc.im>
2021-01-13 22:40:17 +0100

    Webserver and command handling up and running

--- WebSteuerung/WebSteuerung.ino
+++ WebSteuerung/WebSteuerung.ino
@@ -10,38 +10,49 @@ const char *password = "Bettina28101974Martin17071972";
 
 WebServer server(80);
 
-void handleRoot() {
-  digitalWrite(led, 1);
-  char temp[400];
+String buildHTML(int status) {
+  char temp[1000];
   int sec = millis() / 1000;
   int min = sec / 60;
   int hr = min / 60;
 
-  snprintf(temp, 400,
-
+  snprintf(temp, 1000,
            "<html>\
-  <head>\
-    <meta http-equiv='refresh' content='5'/>\
-    <title>ESP32 Demo</title>\
-    <style>\
-      body { background-color: #cccccc; font-family: Arial, Helvetica, Sans-Serif; Color: #000088; }\
-    </style>\
-  </head>\
-  <body>\
-    <h1>Hello from ESP32!</h1>\
-    <p>Uptime: %02d:%02d:%02d</p>\
-    <img src=\"/test.svg\" />\
-  </body>\
-</html>",
-
-           hr, min % 60, sec % 60
+        <head>\
+        <title>ESP32 Railuino</title>\
+        <style>\
+        body { background-color: #cccccc; font-family: Arial, Helvetica, Sans-Serif; Color: #000088; }\
+        </style>\
+        </head>\
+        <body>\
+        <h1>Hello from ESP32!</h1>\
+        <p>Uptime: %02d:%02d:%02d</p>\
+        <h3>Last Command result: %d</h3>\
+        <form id=\"form_12410\" method=\"post\" action=\"/cmd\">\
+        <label for=\"lok\">Lok </label>\
+        <select id=\"lok\" name=\"lok\"> \
+        <option value=\"1\" >1</option>\
+        <option value=\"2\" selected=\"selected\">2</option>\
+        <option value=\"3\" >3</option>\
+        <option value=\"4\" >4</option>\
+        </select></br>\
+        <button type=\"submit\" name=\"func\" value=\"acc\">beschleunigen</button>\
+        <button type=\"submit\" name=\"func\" value=\"dec\">bremsen</button>\
+        <button type=\"submit\" name=\"func\" value=\"turn\">Fahrtrichtung</button>\
+        <button type=\"submit\" name=\"func\" value=\"power\">ein/aus</button>\
+        </form>\
+        </body>\
+        </html>",
+           hr, min % 60, sec % 60, status
           );
-  server.send(200, "text/html", temp);
-  digitalWrite(led, 0);
+  return temp;
+}
+
+void handleRoot() {
+  server.send(200, "text/html", buildHTML(0));
 }
 
 void handleNotFound() {
-  digitalWrite(led, 1);
   String message = "File Not Found\n\n";
   message += "URI: ";
   message += server.uri();
@@ -56,7 +67,6 @@ void handleNotFound() {
   }
 
   server.send(404, "text/plain", message);
-  digitalWrite(led, 0);
 }
 
 void startServer() {
@@ -89,14 +99,9 @@ void startServer() {
 
 TrackControllerInfrared tir = TrackControllerInfrared();
 
-String request;
-String function;
-word arguments[8];
-word numOfArguments;
+String command;
+int function;
 int loco = 2;
-boolean result;
-
-
 
 void setup() {
   Serial.begin(115200);
@@ -107,28 +112,21 @@ void setup() {
   Serial.println("100 Ready");
 }
 
-void handleNotFound() {
-  digitalWrite(led, 1);
-  String message = "File Not Found\n\n";
-  message += "URI: ";
-  message += server.uri();
-  message += "\nMethod: ";
-  message += (server.method() == HTTP_GET) ? "GET" : "POST";
-  message += "\nArguments: ";
-  message += server.args();
-  message += "\n";
-
-  for (uint8_t i = 0; i < server.args(); i++) {
-    message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
-  }
-
-  server.send(404, "text/plain", message);
-  digitalWrite(led, 0);
-}
-
 void receiveRequest() {
-  
-  request = 
+  Serial.print("Command: ");
+  Serial.print(server.method(), BIN);
+  Serial.print(" ");
+  Serial.print(server.uri());
+  Serial.print(" : ");
+  for (int i = 0; i < server.args(); i++) {
+    Serial.print(server.argName(i));
+    Serial.print("=");
+    Serial.print(server.arg(i));
+    Serial.print(", ");
+  }
+  Serial.println();
+  loco = server.arg("lok").toInt();
+  command = server.arg("func");
 }
 
 word stringToWord(String s) {
@@ -141,84 +139,31 @@ word stringToWord(String s) {
   return result;
 }
 
-boolean parse() {
-  int lpar = request.indexOf('(');
-  if (lpar == -1) {
-    function = String(request.substring(0, lpar));
-    return true;
-  }
-
-  function = String(request.substring(0, lpar));
-  function.trim();
-
-  int offset = lpar + 1;
-  int comma = request.indexOf(',', offset);
-  numOfArguments = 0;
-  while (comma != -1) {
-    String tmp = request.substring(offset, comma);
-    tmp.trim();
-    arguments[numOfArguments++] = stringToWord(tmp);
-    offset = comma + 1;
-    comma = request.indexOf(',', offset);
-  }
-
-  int rpar = request.indexOf(')', offset);
-  while (rpar == -1) {
-    return false;
-  }
-
-  if (rpar > offset) {
-    String tmp = request.substring(offset, rpar);
-    tmp.trim();
-    arguments[numOfArguments++] = stringToWord(tmp);
-  }
-
-  return true;
-}
-
 boolean dispatch() {
-  int a = loco;
-  if (numOfArguments != 0 && arguments[0] != loco) {
-    a = arguments[0];
-  }
-
-  if (function == "b") {
-    Serial.println(a);
-    return tir.accelerateLoco(a);
-  } else if (function == "B") {
-    return tir.decelerateLoco(a);
-  } else if (function == "u") {
-    return tir.toggleLocoDirection(a);
-    //  } else if (function == "setLocoDirection") {
+  if (command == "acc") {
+    return tir.accelerateLoco(loco);
+  } else if (command == "dec") {
+    return tir.decelerateLoco(loco);
+  } else if (command == "turn") {
+    return tir.toggleLocoDirection(loco);
+    //  } else if (command == "setLocoDirection") {
     //    return tir.setLocoDirection(a, arguments[1]);
-  } else if (function == "f") {
-    return tir.toggleLocoFunction(a, arguments[1]);
-  } else if (function == "setTurnout") {
-    return tir.setTurnout(a, arguments[1]);
-  } else if (function == "p") {
-    return tir.setPower(a);
-    //  } else if (function == "setLocoSpeed") {
+  } else if (command == "func") {
+    return tir.toggleLocoFunction(loco, function);
+  } else if (command == "set_t") {
+    return tir.setTurnout(loco, function);
+  } else if (command == "power") {
+    return tir.setPower(loco);
+    //  } else if (command == "setLocoSpeed") {
     //    return tir.setLocoSpeed(a, arguments[1]);
-  } else if (function == "l") {
-    loco = arguments[0];
   } else {
     return false;
   }
-
 }
 
 void loop() {
-  //  Serial.print("201 Waiting ... ");
+  //  Serial.println("handleClient");
   server.handleClient();
-  Serial.print("recieved: ");
-  Serial.println(request);
-  if (parse()) {
-    if (dispatch()) {
-      Serial.println("200 Ok");
-    } else {
-      Serial.println("300 Command error");
-    }
-  } else {
-    Serial.println("301 Syntax error");
-  }
+  //  Serial.println("send");
+  server.send(200, "text/html", buildHTML(dispatch()));
 }
